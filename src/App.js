da9b2b8156b877d878f877d89da0c584fa7cb425
Kittiphong <kittiphong@efoodchoice.com>
2022-01-31 17:24:53 +0700

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 
+const Paragraph = styled.div`
+  padding: 1vh 0;
+`
+const Container = styled.div`
+  max-width: 1140px;
+  margin: auto;
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Paragraph>
+        Edit <code>src/App.js</code> and save to reload.
+      </Paragraph>
+    </Container>
   );
 }
 
